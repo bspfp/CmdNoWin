4ed43ecbfdd72e381f1be4e3740dbadcae132112
bspfp <kwanghochoi@hotmail.com>
2023-02-01 15:42:04 +0900

    v1

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+
+namespace CmdNoWin
+{
+    internal static class Program
+    {
+        [STAThread]
+        static int Main(string[] args)
+        {
+            var cmdargs = "";
+            var cmdline = Environment.CommandLine;
+            if (cmdline.StartsWith("\""))
+            {
+                var endq = cmdline.IndexOf('"', 1);
+                if (endq <= 1)
+                    return 1;
+                var starta = endq + 2;
+                cmdargs = cmdline.Substring(starta);
+            }
+            else
+            {
+                for (int i = 1; i < cmdline.Length; i++)
+                {
+                    if (char.IsWhiteSpace(cmdline[i]))
+                    {
+                        cmdargs = cmdline.Substring(i + 1);
+                        break;
+                    }
+                }
+            }
+            if (string.IsNullOrWhiteSpace(cmdargs))
+                return 1;
+
+            var psi = new ProcessStartInfo()
+            {
+                FileName = "cmd.exe",
+                Arguments = $"/c \"{cmdargs}\"",
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            Debug.WriteLine($"CmdArgs: {psi.Arguments}");
+
+            var p = Process.Start(psi);
+            p.WaitForExit();
+            return p.ExitCode;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # CmdNoWin
+
 윈도우 없이 명령 프롬프트에서 명령 실행하기
+
+- 사용법
+ - CmdNoWin.exe <명령>
+ - 예) notepad 실행
+   - CmdNoWin.exe notepad somefile.txt
+- 용도
+ - some.cmd와 같은 배치파일을 실행하거나 DEL 명령 같은 것을 실행하는 바로가기를 만들면 cmd 창이 잠깐이라도 뜨는데 이걸 안 듸우려고
+ - Watch Dogs Legion의 팀원 초상화가 자꾸 깨져서 DEL 명령으로 일괄 지우려는데 자꾸 창이 떠서 만들어 봄
+- Icon: Jojo Mendoza Deleket (from [veryicon](https://www.veryicon.com/icons/application/3d-cartoon/command-prompt-4.html))
