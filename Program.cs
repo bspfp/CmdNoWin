751f0373155ad5a8aee3e2802d8f023deb130482
BS <kwanghochoi@hotmail.com>
2024-11-04 00:43:22 +0900

    v1.1

--- Program.cs
+++ Program.cs
@@ -1,28 +1,38 @@
 ﻿using System;
 using System.Diagnostics;
+using System.IO;
 
 namespace CmdNoWin {
     internal static class Program {
         [STAThread]
         static int Main() {
+            var thisfile = "";
             var cmdargs = "";
             var cmdline = Environment.CommandLine;
             if (cmdline.StartsWith("\"")) {
                 var endq = cmdline.IndexOf('"', 1);
                 if (endq <= 1)
                     return 1;
+                thisfile = cmdline.Substring(1, endq - 1);
                 cmdargs = cmdline.Substring(endq + 1);
             } else {
                 for (int i = 1; i < cmdline.Length; i++) {
                     if (char.IsWhiteSpace(cmdline[i])) {
+                        thisfile = cmdline.Substring(0, i);
                         cmdargs = cmdline.Substring(i + 1);
                         break;
                     }
                 }
             }
             cmdargs = cmdargs.Trim();
-            if (string.IsNullOrWhiteSpace(cmdargs))
+            if (string.Compare(Path.GetFileName(thisfile), "CmdNoWin.exe") != 0) {
+                var name = Path.GetFileNameWithoutExtension(thisfile);
+                var dir = Path.GetDirectoryName(thisfile);
+                cmdargs = Path.Combine(dir, $"_{name}.run.cmd {cmdargs}");
+            }
+            if (string.IsNullOrWhiteSpace(cmdargs)) {
                 return 1;
+            }
 
             var psi = new ProcessStartInfo() {
                 FileName = "cmd.exe",
--- README.md
+++ README.md
@@ -2,11 +2,36 @@
 
 윈도우 없이 명령 프롬프트에서 명령 실행하기
 
-- 사용법
- - CmdNoWin.exe <명령>
- - 예) notepad 실행
-   - CmdNoWin.exe notepad somefile.txt
-- 용도
- - some.cmd와 같은 배치파일을 실행하거나 DEL 명령 같은 것을 실행하는 바로가기를 만들면 cmd 창이 잠깐이라도 뜨는데 이걸 안 듸우려고
- - Watch Dogs Legion의 팀원 초상화가 자꾸 깨져서 DEL 명령으로 일괄 지우려는데 자꾸 창이 떠서 만들어 봄
-- Icon: Jojo Mendoza Deleket (from [veryicon](https://www.veryicon.com/icons/application/3d-cartoon/command-prompt-4.html))
+## 사용 방법 1: 지정한 명령 실행
+
+```batch
+:: CmdNoWin.exe <명령> [<파라미터> ...]
+CmdNoWin.exe dir /s /b ..
+```
+
+## 사용 방법2: 특정 파일 실행
+
+1. 폴더 생성
+1. 폴더 안에 `CmdNoWin.exe`를 복사
+1. `CmdNoWin.exe` 파일명을 원하는 이름으로 변경 (`some.exe`)
+1. `_some.run.cmd` 파일을 생성하여 원하는 스크립트 작성
+1. `some.exe`를 실행하면 `_some.run.cmd` 가 실행됨
+
+
+## Usage Method 1: Execute a Specified Command
+
+```batch
+:: CmdNoWin.exe <command> [<parameter> ...]
+CmdNoWin.exe dir /s /b ..
+```
+
+## Usage Method 2: Execute a Specific File
+
+1. Create a folder
+1. Copy `CmdNoWin.exe` into the folder
+1. Rename `CmdNoWin.exe` to your desired name (`some.exe`)
+1. Create a `_some.run.cmd` file and write your desired script
+1. When you run `some.exe`, `_some.run.cmd` will be executed.
+   ```batch
+   some.exe [parameter ...]
+   ```
